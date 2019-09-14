fcc3c434186f65b259042324d8931565c9d27c3a
Yuuki1337 <teissier.remy@gmail.com>
2019-09-14 17:26:16 +0200

    tryagain

--- Angular_Resume_Front/src/app/app.module.ts
+++ Angular_Resume_Front/src/app/app.module.ts
@@ -7,14 +7,12 @@ import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
-import { HomepageComponent } from './homepage/homepage.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
     SearchBoxComponent,
     TestComponent
   ],
